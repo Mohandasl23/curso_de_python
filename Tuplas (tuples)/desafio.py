8329b68971228a6ddc621bd58e1e77fbbf398e75
Mohandasl23 <mohandasdealbuquerque@gmail.com>
2026-07-06 07:42:54 -0300

    Hoje estudei aua 19 e pratique um desafio atrelado a aula 19

---
+++ Tuplas (tuples)/desafio.py
@@ -0,0 +1,8 @@
+tupla = ("maça","banana", "laranja")
+for fruta in tupla:
+    print(fruta)
+
+quantidade_frutas = len(tupla)
+print("Quantidade de frutas na tupla:", quantidade_frutas)    
+
+# Desafio: Crie um programa para emular uma quitada. Cire uma tupla para armazenar as palavas "maça", "banana", "laranja". Imprima cada um dos nomes das frutas, utilizado a tupla em uma estrutura do tipo for. Mostre a quantidade de frutas que existem na tupla.
---
+++ Tuplas (tuples)/ex_19.py
@@ -0,0 +1,12 @@
+tupla = (0,1,3, 3, 5, 7, 7,7)
+
+print("Tupla original:", tupla)
+
+qtde_elementos = len(tupla)
+print("Quantidade de elementos na tupla:", qtde_elementos)
+
+qtde_setes = tupla.count(7)
+print("Quantidade de elementos 7 na tupla:", qtde_setes)
+
+mostrar_elemento_4 = tupla[4]
+print("Elemento na posição 4:", mostrar_elemento_4)
